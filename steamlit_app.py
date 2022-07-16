a6d9aa5599227c06bdfa2fb48f46a728c4a77774
madhudba3009 <109396502+madhudba3009@users.noreply.github.com>
2022-07-16 07:59:25 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -6,3 +6,8 @@ streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🍌🥭  Omega 3 & Blueberry Oatmeal')
 streamlit.text('Har-Biled free-Range egg')
+
+import pandas
+
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+stramlimit.dataframe(my_fruit_list)

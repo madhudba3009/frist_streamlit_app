49968fe52e8eedf7f607f1bf01f6135b2c87df35
madhudba3009 <109396502+madhudba3009@users.noreply.github.com>
2022-07-16 07:30:00 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My parents New Healthy Diner')
 
 
 streamlit.header('Breakfast Menu')
-stramlit.text('Omega 3 & Blueberry Oatmeal')
-stramlit.text('Har-Biled free-Range egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Har-Biled free-Range egg')

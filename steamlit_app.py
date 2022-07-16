cd7e9f35e9fbe8c474b50db94f7f9bad437fe90e
madhudba3009 <109396502+madhudba3009@users.noreply.github.com>
2022-07-16 08:01:10 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -10,4 +10,4 @@ streamlit.text('Har-Biled free-Range egg')
 import pandas
 
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-stramlimit.dataframe(my_fruit_list)
+streamlimit.dataframe(my_fruit_list)
